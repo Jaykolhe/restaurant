2515965251c0784703fa21c309241a9e06ec9bfd
Jay Kolhe <kolhejay6@gmail.com>
2025-02-04 12:16:29 +0530

    Created new method to getAllRestaurants

--- src/main/java/com/jay/service/RestaurantService.java
+++ src/main/java/com/jay/service/RestaurantService.java
@@ -1,8 +1,13 @@
 package com.jay.service;
 
+import com.jay.model.Response.RestaurantResponse;
 import com.jay.model.RestaurantDto;
+
+import java.util.List;
 
 public interface RestaurantService {
 
     boolean addRestaurant(RestaurantDto restaurantDto);
+
+    List<RestaurantResponse> getAllRestaurants();
 }

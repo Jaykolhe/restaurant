50c95929d56fe946fcfac29222a091db595f2fc4
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 15:27:53 +0530

    Created and implemented findAddressById method

--- src/main/java/com/jay/service/RestaurantAddressService.java
+++ src/main/java/com/jay/service/RestaurantAddressService.java
@@ -1,4 +1,12 @@
 package com.jay.service;
 
+import com.jay.model.ResaurantAddressDto;
+import com.jay.model.Response.RestaurantAddressResponse;
+
 public interface RestaurantAddressService {
+
+    public boolean addAddress(ResaurantAddressDto resaurantAddressDto);
+
+    public RestaurantAddressResponse getAddressById(int id);
+
 }
--- src/main/java/com/jay/service/serviceImpl/RestaurantAddressServiceImpl.java
+++ src/main/java/com/jay/service/serviceImpl/RestaurantAddressServiceImpl.java
@@ -1,8 +1,69 @@
 package com.jay.service.serviceImpl;
 
+import com.jay.entity.Restaurant;
+import com.jay.entity.RestaurantAddress;
+import com.jay.exceptions.RestaurantException;
+import com.jay.model.ResaurantAddressDto;
+import com.jay.model.Response.RestaurantAddressResponse;
+import com.jay.repository.RestaurantAddressRepository;
+import com.jay.repository.RestaurantRepository;
 import com.jay.service.RestaurantAddressService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RestaurantAddressServiceImpl implements RestaurantAddressService {
+
+    private final RestaurantRepository restaurantRepository;
+    private final RestaurantAddressRepository restaurantAddressRepository;
+
+
+
+    @Override
+    public boolean addAddress(ResaurantAddressDto resaurantAddressDto) {
+
+        Restaurant restaurant = restaurantRepository.findById(resaurantAddressDto.getRestro_id())
+                                                    .orElseThrow(()-> new RestaurantException("Restaurant Not Found"));
+
+        RestaurantAddress address = mapDtoToEntity(resaurantAddressDto,restaurant);
+        restaurantAddressRepository.save(address);
+
+        return true;
+    }
+
+    @Override
+    public RestaurantAddressResponse getAddressById(int id) {
+        RestaurantAddress restaurantAddress = restaurantAddressRepository.findById(id)
+                .orElseThrow(()-> new RestaurantException("Address not found with id"));
+
+        return mapEntityToDto(restaurantAddress);
+
+    }
+
+    private RestaurantAddressResponse  mapEntityToDto(RestaurantAddress address) {
+        return  RestaurantAddressResponse.builder()
+                        .addressLine1(address.getAddressLine1())
+                .addressLine2(address.getAddressLine2())
+                .city(address.getCity())
+                .state(address.getState())
+                .country(address.getCountry())
+                .pincode(address.getPincode())
+                .restaurantName(address.getRestaurant().getName())
+                .build();
+
+    }
+
+    private RestaurantAddress mapDtoToEntity(ResaurantAddressDto resaurantAddressDto, Restaurant restaurant) {
+        return RestaurantAddress.builder()
+                        .addressLine1(resaurantAddressDto.getAddressLine1())
+                        .addressLine2(resaurantAddressDto.getAddressLine2())
+                        .city(resaurantAddressDto.getCity())
+                        .state(resaurantAddressDto.getState())
+                        .country(resaurantAddressDto.getCountry())
+                        .pincode(resaurantAddressDto.getPincode())
+                        .restaurant(restaurant)
+                        .build();
+
+    }
 }

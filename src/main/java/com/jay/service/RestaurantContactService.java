84cfbad22294b18fed122276ca6c09e8a8a809f2
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 20:49:55 +0530

    Created Contactservice class and Implemented addContact method

--- src/main/java/com/jay/service/RestaurantContactService.java
+++ src/main/java/com/jay/service/RestaurantContactService.java
@@ -1,4 +1,11 @@
 package com.jay.service;
 
+import com.jay.entity.Restaurant;
+import com.jay.entity.RestaurantContact;
+import com.jay.model.RestaurantContactDto;
+
 public interface RestaurantContactService {
+
+
+    public boolean addRestaurantContact(RestaurantContactDto restaurantContactDto,int address_id);
 }
--- src/main/java/com/jay/service/serviceImpl/RestaurantContactServiceImpl.java
+++ src/main/java/com/jay/service/serviceImpl/RestaurantContactServiceImpl.java
@@ -1,8 +1,40 @@
 package com.jay.service.serviceImpl;
 
+import com.jay.entity.RestaurantAddress;
+import com.jay.entity.RestaurantContact;
+import com.jay.exceptions.RestaurantException;
+import com.jay.model.RestaurantContactDto;
+import com.jay.repository.RestaurantAddressRepository;
+import com.jay.repository.RestaurantContactRepository;
 import com.jay.service.RestaurantContactService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RestaurantContactServiceImpl  implements RestaurantContactService {
+
+
+    private final RestaurantContactRepository restaurantContactRepository;
+
+    private final RestaurantAddressRepository restaurantAddressRepository;
+
+
+    @Override
+    public boolean addRestaurantContact(RestaurantContactDto restaurantContactDto, int address_id) {
+        RestaurantAddress restaurantAddress = restaurantAddressRepository.findById(address_id)
+                .orElseThrow(()-> new RestaurantException("Restaurant Address is not Found with given Id"));
+
+        RestaurantContact restaurantContact = new RestaurantContact();
+        restaurantContact.setRestaurantAddress(restaurantAddress);
+        restaurantContact.setMobile(restaurantContactDto.getMobile());
+        restaurantContact.setEmail(restaurantContact.getEmail());
+
+        restaurantContactRepository.save(restaurantContact);
+
+       return true;
+
+    }
+
+
 }

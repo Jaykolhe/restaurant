b45d5c37854e25632406673fb9a0e942dbe83c67
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 19:33:34 +0530

    Created Restaurantowner Relationship method and Implemented it

--- src/main/java/com/jay/service/RestroOwnerRelationshipService.java
+++ src/main/java/com/jay/service/RestroOwnerRelationshipService.java
@@ -1,4 +1,14 @@
 package com.jay.service;
 
+import com.jay.entity.Restaurant;
+
+import java.util.List;
+
 public interface RestroOwnerRelationshipService {
+
+
+    public boolean addOwnerToRestaurant(Integer owner_id, Integer restro_id);
+
+
+
 }

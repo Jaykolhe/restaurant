e1699587ad19ec7c09230c337368790dcb500fa3
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 12:32:59 +0530

    Integrated Swagger into Application

---
+++ src/main/java/com/jay/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.jay.config;
+
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+
+    @Bean
+    public OpenAPI swaggerUISetup(){
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Restaurant Backend Application ")
+                                .description("Mini Project")
+                );
+    }
+}

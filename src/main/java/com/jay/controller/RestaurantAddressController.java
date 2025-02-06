ba929cfce1feb9030835d30ff3dbdea81a6e2c50
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 15:26:46 +0530

    Created RestaurantAddress Controller

---
+++ src/main/java/com/jay/controller/RestaurantAddressController.java
@@ -0,0 +1,41 @@
+package com.jay.controller;
+
+
+import com.jay.model.ResaurantAddressDto;
+import com.jay.model.Response.RestaurantAddressResponse;
+import com.jay.service.RestaurantAddressService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/restaurant/address")
+public class RestaurantAddressController {
+
+
+    private final RestaurantAddressService restaurantAddressService;
+
+
+    public RestaurantAddressController(RestaurantAddressService restaurantAddressService) {
+        this.restaurantAddressService = restaurantAddressService;
+    }
+
+
+    @PostMapping("/addAddress")
+    public ResponseEntity<String>  addAddresss(@RequestBody ResaurantAddressDto resaurantAddressDto ){
+
+
+        restaurantAddressService.addAddress(resaurantAddressDto);
+
+        return new ResponseEntity<>("Restaurant Address Added", HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantAddressResponse> getRestaurantAddressById(@PathVariable int id){
+
+        RestaurantAddressResponse restaurantAddressResponse = restaurantAddressService.getAddressById(id);
+
+        return new ResponseEntity<>(restaurantAddressResponse,HttpStatus.OK);
+    }
+}

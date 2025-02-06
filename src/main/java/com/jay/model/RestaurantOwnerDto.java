ff4b210512c579684e2f08f46b9d2090c440826a
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 18:59:57 +0530

    Created Restaurant OwnerDto class

--- src/main/java/com/jay/model/RestaurantOwnerDto.java
+++ src/main/java/com/jay/model/RestaurantOwnerDto.java
@@ -1,4 +1,21 @@
 package com.jay.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RestaurantOwnerDto {
+
+
+    private int owner_id;
+    private String name;
+    private String passport;
+    private String email;
+    private String mobile;
+
 }

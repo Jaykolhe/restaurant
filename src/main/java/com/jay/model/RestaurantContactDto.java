7efb535a5663522ac8e256fe55bbb90bfba4a97f
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 20:47:35 +0530

    Created ContactDto class

--- src/main/java/com/jay/model/RestaurantContactDto.java
+++ src/main/java/com/jay/model/RestaurantContactDto.java
@@ -1,4 +1,20 @@
 package com.jay.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RestaurantContactDto {
+
+         private int contact_id;
+
+         private int address_id;
+
+         private String mobile;
+
+         private String email;
+
+
 }

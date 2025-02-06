7abe4e9b401a49f11d2da81c5dac6d2e226cf1d5
Jay Kolhe <kolhejay6@gmail.com>
2025-02-06 18:58:41 +0530

    Created Restaurant Owner Response class

---
+++ src/main/java/com/jay/model/Response/RestaurantOwnerResponse.java
@@ -0,0 +1,19 @@
+package com.jay.model.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class RestaurantOwnerResponse {
+
+    private String name;
+    private String passport;
+    private String email;
+    private String mobile;
+
+}

f73d312edbec7185db27b75cf1ecdf08686fedbc
Jay Kolhe <kolhejay6@gmail.com>
2025-02-07 12:31:20 +0530

    Added Lombok Annotations

--- src/main/java/com/jay/entity/RestaurantOwner.java
+++ src/main/java/com/jay/entity/RestaurantOwner.java
@@ -1,11 +1,11 @@
 package com.jay.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
